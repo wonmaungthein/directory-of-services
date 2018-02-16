dd6b8d4fb8d5c8524b6366485aedb0d3b4b5f9e3
yevhensydorov <assid@ukr.net>
2018-02-16 11:32:12 +0000

    add organisation database

---
+++ directory-of-services/database/organisation.sql
@@ -0,0 +1,50 @@
+PRAGMA foreign_keys = ON;
+
+-- ORGANISATION
+
+create table organisation (
+	org_id 									integer not null primary key,
+	org_name 								varchar,
+	website 								varchar
+);
+
+-- BRANCH
+
+create table branch (
+	branch_id 							varchar not null primary key,
+	org_id 									varchar not null,
+	borough 								varchar,
+	foreign key(org_id) 		references organisation(org_id)
+);
+
+-- SERVICE
+
+create table service (
+	branch_id 							varchar not null,
+	service_name 						varchar,
+	service_days 						varchar not null,
+	process 								datetime not null,
+	foreign key(branch_id) 	references branch(branch_id)
+);
+
+-- ADRESS
+
+create table adress (
+	adress_id 							varchar primary key,
+	branch_id 							varchar not null,
+	telephone 							varchar,
+	adress_line 						varchar,
+	city 										varchar,
+	postcode 								varchar,
+	zone 										varchar,
+	foreign key(branch_id) references branch(branch_id)
+);
+
+-- LOCATION
+
+create table location (
+	adress_id 							varchar not null,
+	lat 										varchar,
+	lng 										varchar,
+	foreign key(adress_id) 	references adress(adress_id)
+);
