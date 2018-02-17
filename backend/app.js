d6bb6c50380d05e1faf34b45fe31a17a062a05ba
MrDwina <diwina1993@hotmail.com>
2018-02-17 23:44:03 +0000

    removed unnecessary depances and view directory and added morgan logger, morgan debug

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,8 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var path = require('path')
+const morgan = require('morgan');
+var morganDebug = require('morgan-debug');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,16 +10,13 @@ var router = require('./routes/api');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// Show requestes with morgan
+app.use(morgan('combined'));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//Morgan debug
+app.use(morganDebug('backend', 'combined'));
+
+// static view
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "DOS backend" });
+  res.json({ title: "DOS backend" });
 });
 
 module.exports = router;
