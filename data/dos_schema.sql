f568395d5d817823f355db44aec6d2e906fb72fa
Kash <kash.karimi@gmail.com>
2018-02-19 10:00:45 +0000

    fix spaces, process->varchar

--- data/dos_schema.sql
+++ data/dos_schema.sql
@@ -3,9 +3,9 @@ PRAGMA foreign_keys = ON;
 -- ORGANISATION
 
 create table organisation (
-	org_id 									integer primary key,
-	org_name 								varchar,
-	website 								varchar,
+    org_id 								integer primary key,
+    org_name 								varchar,
+    website 								varchar,
     email_address 							varchar,
     telephone 								varchar
 );
@@ -13,19 +13,19 @@ create table organisation (
 -- BRANCH
 
 create table branch (
-	branch_id 								varchar not null primary key,
-	org_id 									varchar not null,
-	borough 								varchar,
+	branch_id 							varchar not null primary key,
+	org_id 								varchar not null,
+	borough 							varchar,
 	foreign key(org_id) 					references organisation(org_id)
 );
 
 -- SERVICE
 
 create table service (
-	branch_id 								varchar not null,
+	branch_id 							varchar not null,
 	service_name 							varchar,
 	service_days 							varchar not null,
-	process 								datetime not null,
+	process 							varchar not null,
 	foreign key(branch_id) 					references branch(branch_id)
 );
 
@@ -34,19 +34,19 @@ create table service (
 -- ADRESS
 
 create table adress (
-	adress_id 								varchar primary key,
-	branch_id 								varchar not null,
+	adress_id 							varchar primary key,
+	branch_id 							varchar not null,
 	adress_line 							varchar,
-	city 									varchar,
-	postcode 								varchar,
+	city 								varchar,
+	postcode 							varchar,
 	foreign key(branch_id) 					references branch(branch_id)
 );
 
 -- LOCATION
 
 create table location (
-	adress_id 								varchar not null,
-	lat 									varchar,
-	lng 									varchar,
+	adress_id 							varchar not null,
+	lat 								varchar,
+	lng 								varchar,
 	foreign key(adress_id) 					references adress(adress_id)
 );
