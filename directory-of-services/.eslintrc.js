8167a46cfab771b82d9324c6d6336c51f93e61ef
MrDwina <diwina1993@hotmail.com>
2018-02-19 22:07:45 +0000

    created frontend eslint

---
+++ directory-of-services/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "extends": ["airbnb", "prettier"],
+    "parser": "babel-eslint",
+    "plugins": ["flowtype"],
+    "rules": {
+        "jsx-a11y/anchor-is-valid": [
+            "error",
+            {
+                "components": [],
+                "specialLink": ["to"],
+                "aspects": ["noHref", "invalidHref", "preferButton"]
+            }
+        ],
+        "react/forbid-prop-types": "off",
+        "react/jsx-filename-extension": [
+            1,
+            {
+                "extensions": [".js", ".jsx"]
+            }
+        ],
+        "import/extensions": "off",
+        "import/no-extraneous-dependencies": "off",
+        "import/no-unresolved": "off"
+    },
+    "settings": {
+        "import/resolver": {
+            "node": {
+                "paths": ["src/"]
+            }
+        }
+    },
+    "env": {
+        "browser": true,
+        "node": true,
+        "jest": true,
+        "mocha": true,
+        "es6": true
+    }
+}
