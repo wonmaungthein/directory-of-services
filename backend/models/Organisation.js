b22661a4a1bea0ba4fd97656450d5668b604e594
Kash <kash.karimi@gmail.com>
2018-02-21 13:28:05 +0000

    Per es6

--- backend/models/Organisation.js
+++ backend/models/Organisation.js
@@ -1,32 +1,22 @@
 import { Model } from 'objection';
 
 export default class Organisation extends Model {
-  static tableName = 'organisation';
-  // static jsonSchema = {
-  //   type: 'object',
-  //   required: ['org_id'],
+  static get tableName() {
+    return 'organisation';
+  }
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['org_id'],
 
-  //   properties: {
-  //     org_id: { type: 'integer' },
-  //     ownerId: { type: ['integer', 'null'] },
-  //     org_name: { type: 'string', minLength: 1, maxLength: 255 },
-  //     website: { type: 'string', minLength: 1, maxLength: 255 },
-  //     email_address: { type: 'string', minLength: 1, maxLength: 255 },
-  //     telephone: { type: 'string', minLength: 1, maxLength: 255 },
-  //   },
-
-  // This object defines the relations to other models.
-  static relationMappings = {
-    owner: {
-      relation: Model.BelongsToOneRelation,
-      // The related model. This can be either a Model subclass constructor or an
-      // absolute file path to a module that exports one. We use the file path version
-      // here to prevent require loops.
-      modelClass: `${__dirname}/Person`,
-      join: {
-        from: 'Animal.ownerId',
-        to: 'Person.id',
+      properties: {
+        org_id: { type: 'integer' },
+        ownerId: { type: ['integer', 'null'] },
+        org_name: { type: 'string', minLength: 1, maxLength: 255 },
+        website: { type: 'string', minLength: 1, maxLength: 255 },
+        email_address: { type: 'string', minLength: 1, maxLength: 255 },
+        telephone: { type: 'integer', minLength: 1, maxLength: 16 },
       },
-    },
-  };
+    };
+  }
 }
