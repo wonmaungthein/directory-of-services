5ee1fb22b16a4c8d4688f2ff287fd5a148f865dc
Kash <kash.karimi@gmail.com>
2018-02-23 20:00:59 +0000

    initialise eb

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path')
+const path = require('path');
 const morgan = require('morgan');
 
 const router = require('./routes/api');
@@ -16,21 +16,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', router);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
