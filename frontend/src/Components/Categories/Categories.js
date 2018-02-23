464959d98ba26bcaaa663460fa018cc619a53406
MrDwina <diwina1993@hotmail.com>
2018-02-23 10:42:20 +0000

    adde prettier config file and fixed frontend lint error

--- frontend/src/Components/Categories/Categories.js
+++ frontend/src/Components/Categories/Categories.js
@@ -3,34 +3,27 @@ import { List, ListItem } from 'material-ui/List';
 import { Link } from "react-router-dom";
 import './categories.css'
 
-export default class Categories extends React.Component {
+const Categories=() => (
+  <div>
+    <List className="categories">
+      <ListItem
+        className="category"
+        primaryText="Categories"
+        initiallyOpen={false}
+        nestedItems={[
+          <Link to="/debt">Debt</Link>,
+          <Link to="/ypfamilies">YP Families</Link>,
+          <Link to="/womendv">Women DV</Link>,
+          <Link to="/trafficking">Trafficking</Link>,
+          <Link to="/destitution">Destitution</Link>,
+          <Link to="/lgbtqi">LGBTQI</Link>,
+          <Link to="/mentalhealthservices">Mental Health Services</Link>,
+          <Link to="/healthcare">Healthcare</Link>,
+          <Link to="/womendv">Women DV</Link>
+        ]}
+      />
+    </List>
 
-  state = {
-    open: false,
-  };
-
-  render() {
-    return (
-      <div>
-        <List className="categories">
-          <ListItem className="category"
-            primaryText="Categories"
-            initiallyOpen={false}
-            nestedItems={[
-              <Link to="/debt">Debt</Link>,
-              <Link to="/ypfamilies">YP Families</Link>,
-              <Link to="/womendv">Women DV</Link>,
-              <Link to="/trafficking">Trafficking</Link>,
-              <Link to="/destitution">Destitution</Link>,
-              <Link to="/lgbtqi">LGBTQI</Link>,
-              <Link to="/mentalhealthservices">Mental Health Services</Link>,
-              <Link to="/healthcare">Healthcare</Link>,
-              <Link to="/womendv">Women DV</Link>
-            ]}
-          />
-        </List>
-
-      </div>
-    );
-  }
-}
+  </div>
+);
+export default Categories
--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,24 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 import { Link } from "react-router-dom";
 import './home.css'
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                <div className="main-nav">
-                    <div className="orgonaization-title">
-                        <span>HEALTHCARE</span>
-                        <Link to="/addOrgonaization" className="add-orgnaization"><span className="add-orgonaization-button">Add new</span><ContentAdd className="button-icon" /></Link>
-                    </div>
-                    <div className="login-section">
-                        <h4>Loggedin as <strong>Carmela</strong></h4>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+const Home = () =>(
+  <div>
+    <div className="main-nav">
+      <div className="orgonaization-title">
+        <span>HEALTHCARE</span>
+        <Link to="/addOrgonaization" className="add-orgnaization"><span className="add-orgonaization-button">Add new</span><ContentAdd className="button-icon" /></Link>
+      </div>
+      <div className="login-section">
+        <h4>Loggedin as <strong>Carmela</strong></h4>
+      </div>
+    </div>
+  </div>
+);
+
 
 export default Home;
