fd3e9fbc96a2c3d0aa460300baabd29bdbf44b43
MrDwina <diwina1993@hotmail.com>
2018-03-01 12:38:34 +0000

    created data filed to get some of organisation as json data and use it in organisation card

--- frontend/src/Components/Organisation/index.js
+++ frontend/src/Components/Organisation/index.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import EditOrganisation from './EditOrganisation';
 import SingleOrganisation from './SingleOrganisation';
-import './organisation.css'
+import './organisation.css';
+import orgData from './org_data';
 
 const OrganisationCard = () => (
   <div className="single-orgonaization">
-    <div className="form-card">
-      <EditOrganisation />
-      <SingleOrganisation />
-      <div className="single-orgonaization-details">
-        <h4>Haringey Migrant Service</h4>
-        <h6>Area: North | Borough: Haringey</h6>
-        <div className="health-advice-process">
-          <p> - Health advice</p>
-          <p> - Help accessing NHS</p>
+    {orgData.map(org => {
+      return (
+        <div className="form-card" key={org.id}>
+          <EditOrganisation />
+          <SingleOrganisation />
+          <div className="single-orgonaization-details">
+            <h4>{org.org_name}</h4>
+            <h6>Area: {org.area} | Borough: {org.borough}</h6>
+            <div className="health-advice-process">
+              <p> - Health advice</p>
+              <p> - {org.advice}</p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    <div className="form-card">
-      <EditOrganisation />
-      <SingleOrganisation />
-      <div className="single-orgonaization-details">
-        <h4>Haringey Migrant Service</h4>
-        <h6>Area: North | Borough: Haringey</h6>
-        <div className="health-advice-process">
-          <p> - Health advice</p>
-          <p> - Help accessing NHS</p>
-        </div>
-      </div>
-    </div>
+      );
+    })}  
   </div>
 )
 
---
+++ frontend/src/Components/Organisation/org_data.js
@@ -0,0 +1,32 @@
+const orgData = [
+  {
+    'id': 1,
+    'org_name': 'Haringey Migrant Service',
+    'area': 'North',
+    'borough': 'Haringey',
+    'advice': 'Help accessing NHS',
+  },
+  {
+    'id': 2,
+    'org_name': 'Haringey Migrant Service',
+    'area': 'North',
+    'borough': 'Haringey',
+    'advice': 'Help accessing NHS',
+  },
+  {
+    'id': 3,
+    'org_name': 'Haringey Migrant Service',
+    'area': 'North',
+    'borough': 'Haringey',
+    'advice': 'Help accessing NHS',
+  },
+  {
+    'id': 4,
+    'org_name': 'Haringey Migrant Service',
+    'area': 'North',
+    'borough': 'Haringey',
+    'advice': 'Help accessing NHS',
+  }
+];
+
+export default orgData;
