ce4c7402f4cd2bde958f50929bab694f2bf6a320
MrDwina <diwina1993@hotmail.com>
2018-03-04 14:48:38 +0000

    removed unecessary package, not include in material-ui@next

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import SideBar from "./Components/SideBar/SideBar";
 import Home from './Components/Home'
 import OrganisationCard from './Components/Organisation'
@@ -8,7 +7,6 @@ import AddOrganisationForm from './Components/Organisation/AddOrganisationForm'
 
 const Routes = () => (
   <Router>
-    <MuiThemeProvider>
       <div className="content">
         <SideBar />
         <main className="main-contents">
@@ -17,7 +15,6 @@ const Routes = () => (
           <Route path="/addorganisationForm" component={AddOrganisationForm} />
         </main>
       </div>
-    </MuiThemeProvider>
   </Router>
 );
 
