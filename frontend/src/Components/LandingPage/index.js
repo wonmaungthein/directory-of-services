6ddaec39e15f26b2a30ba6c483fb2d7041911b97
coderyourfuture <you@example.com>
2018-03-07 21:40:40 +0000

    Link added to LandingPage

--- frontend/src/Components/LandingPage/index.js
+++ frontend/src/Components/LandingPage/index.js
@@ -1,23 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './landing-page.css';
 
 const LandingPage = () => (
   <div className="landing-page">
     <div>
       <h1>LOGO</h1>
-
       <div className="content">
         <h3>You are logged out</h3>
-
-        <div className="google-sign">
-          <div className="google-logo">
-            <img src="/google-logo.png" alt="google-logo" />
+        <Link to="/organisations/add">
+          <div className="google-sign">
+            <div className="google-logo">
+              <img src="/google-logo.png" alt="google-logo" />
+            </div>
+            <div className="google-title">
+              <h5>Sign in with google</h5>
+            </div>
           </div>
-
-          <div className="google-title">
-            <h5>Sign in with google</h5>
-          </div>
-        </div>
+        </Link>
       </div>
     </div>
   </div>
