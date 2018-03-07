7df4648fac307af6cba9777f39d6936b9b51bf6b
coderyourfuture <you@example.com>
2018-03-07 13:18:21 +0000

    Landing_page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,22 +5,41 @@ import SideBar from "./Components/SideBar/SideBar";
 import Home from './Components/Home'
 import OrganisationCard from './Components/Organisation'
 import AddOrganisationForm from './Components/Organisation/AddOrganisationForm'
-
 import "./App.css";
+import LandingPage from "./Components/LandingPage";
+
+
 
 const App = () => (
+
   <Router>
+
     <MuiThemeProvider>
+
+      <Route path="/landingpage" component={LandingPage} />
+
       <div className="content">
+
         <SideBar />
-        <main className="main-contents">
+
+        <main className="main-contents">  
+
           <Home />
+
           <Route path="/healthcare" component={OrganisationCard} />
+
           <Route path="/addorganisationForm" component={AddOrganisationForm} />
+
         </main>
+
       </div>
+
     </MuiThemeProvider>
+
   </Router>
+
 );
+
+
 
 export default App;
---
+++ frontend/src/Components/LandingPage/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import './landing-page.css';
+
+
+
+const LandingPage = () => (
+
+  <div className="landing-page">
+
+    <div>
+
+      <h1>LOGO</h1>
+
+      <div className="content">
+
+        <h3>You are logged out</h3>
+
+        <div className='google-sign'>
+
+          <div className="google-logo">
+
+            <img src="/google-logo.png" alt="google-logo" />
+
+          </div>
+
+          <div className="google-title">
+
+            <h5>Sign in with google</h5>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+);
+
+
+
+
+
+export default LandingPage;
