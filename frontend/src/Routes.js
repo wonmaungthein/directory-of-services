62d5d863886930cbc37abdae0e28ac6e6ed44a4e
coderyourfuture <you@example.com>
2018-03-07 17:17:38 +0000

    Landing page route

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -4,13 +4,14 @@ import SideBar from "./Components/SideBar/SideBar";
 import Home from './Components/Home'
 import OrganisationCard from './Components/Organisation'
 import AddOrganisationForm from './Components/Organisation/AddOrganisationForm'
-
+import LandingPage from './Components/LandingPage'
 const Routes = () => (
   <Router>
     <div className="content">
       <SideBar />
       <main className="main-contents">
         <Home />
+        <Route path="/" component={LandingPage} />
         <Route path="/services/healthcare" component={OrganisationCard} />
         <Route path="/organisations/add" component={AddOrganisationForm} />
       </main>
