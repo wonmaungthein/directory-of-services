649650268cb6b6b7d27251e61f1b8fb6a6b8ad3b
MrDwina <diwina1993@hotmail.com>
2018-03-08 16:51:08 +0000

    created drwer instead of sideBar

--- frontend/src/Components/SideBar/index.js
+++ frontend/src/Components/SideBar/index.js
@@ -1,19 +1,127 @@
-import React from 'react';
+
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui/styles';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import List from 'material-ui/List';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import Hidden from 'material-ui/Hidden';
+import Divider from 'material-ui/Divider';
+import MenuIcon from 'material-ui-icons/Menu';
 import { Link } from 'react-router-dom';
 import Categories from '../Categories';
 import UsersMenu from '../Users/UsersMenu';
 import './side-bar.css';
 
-const SideBar = () => (
-  <nav className="mdc-drawer mdc-drawer--permanent mdc-typography">
-    <div className="logo">
-      <Link to="/">LOGO</Link>
-    </div>
-    <Categories />
-    <br />
-    <br />
-    <UsersMenu />
-  </nav>
-);
+const drawerWidth = 240;
 
-export default SideBar;
+const styles = theme => ({
+  appBar: {
+    position: 'absolute',
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+    [theme.breakpoints.up('md')]: {
+      position: 'relative',
+    },
+    background: '#000',
+  },
+});
+
+class SideBar extends React.Component {
+  state = {
+    mobileOpen: false,
+  };
+
+  handleDrawerToggle = () => {
+    this.setState({ mobileOpen: !this.state.mobileOpen });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+
+    const drawer = (
+      <div>
+        <div className={classes.toolbar} />
+        <div className="logo">
+          <Link to="/">LOGO</Link>
+        </div>
+        <Divider />
+        <List>
+          <Categories />
+        </List>
+        <Divider />
+        <List>
+          <UsersMenu />
+        </List>
+      </div>
+    );
+
+    return (
+      <Fragment>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={this.handleDrawerToggle}
+              className={classes.navIconHide}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" noWrap>
+              Refugees Info
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Hidden mdUp>
+          <Drawer
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={this.state.mobileOpen}
+            onClose={this.handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <Drawer
+            variant="permanent"
+            open
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </Fragment>
+    );
+  }
+}
+
+SideBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(SideBar);
