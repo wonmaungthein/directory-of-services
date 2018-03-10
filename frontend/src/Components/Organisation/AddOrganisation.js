50d47f3777f1877168f3c4ced62d029390093861
MrDwina <diwina1993@hotmail.com>
2018-03-10 15:35:04 +0000

    removed the validation code

--- frontend/src/Components/Organisation/AddOrganisation.js
+++ frontend/src/Components/Organisation/AddOrganisation.js
@@ -6,99 +6,31 @@ import TopNav from '../TopNav';
 export default class AddOrganisation extends Component {
   state = {
     name: "",
-    nameError: "",
     area: "",
-    areaError: "",
     borough: "Highbury and Islington",
-    boroughError: "",
     process: "Email, then call",
-    processError: "",
     day: "Monday",
-    dayError: "",
     telephone: "028 297 4111",
-    telephoneError: "",
     email: "",
-    emailError: "",
     website: "",
-    websiteError: "",
     service: "",
-    serviceError: "",
     category: [],
   };
 
-  validate = () => {
-    let isError = false;
-    const errors = {
-      nameError: "",
-      areaError: "",
-      boroughError: "",
-      processError: "",
-      dayError: "",
-      telephoneError: "",
-      emailError: "",
-      websiteError: "",
-      serviceError: "",
-    }
-    if (this.state.name.length < 2) {
-      isError = true;
-      errors.nameError = "The name has to be at least two character"
-    }
-    if (this.state.areaError.length < 0) {
-      isError = true;
-      errors.nameError = "The area filed is required"
-    }
-    if (this.state.boroughError.length < 0) {
-      isError = true;
-      errors.nameError = "The borough filed is required"
-    }
-    if (this.state.processError.length < 0) {
-      isError = true;
-      errors.nameError = "The process filed is required"
-    }
-
-    if (this.state.dayError.length < 0) {
-      isError = true;
-      errors.nameError = "The day field is required"
-    }
-    if (this.state.telephoneError.length < 10) {
-      isError = true;
-      errors.nameError = "The telephone has to be at least 10 numbers"
-    }
-    if (this.state.emailError.length < 0) {
-      isError = true;
-      errors.nameError = "Invalid email"
-    }
-    if (this.state.websiteError.length < 0) {
-      isError = true;
-      errors.nameError = "The website field is required"
-    }
-    if (this.state.serviceError.length < 0) {
-      isError = true;
-      errors.nameError = "The service field is required"
-    }
-    this.setState({
-      ...this.state,
-      ...errors,
-    });
-    return isError;
-  }
   handleSubmit = (e) => {
     e.preventDefault();
-    const err = this.validate();
-    if (!err) {
-      this.setState({
-        area: "",
-        borough: "",
-        name: "",
-        process: "",
-        day: "",
-        telephone: "",
-        email: "",
-        website: "",
-        service: "",
-        category: []
-      })
-    }
+    this.setState({
+      area: "",
+      borough: "",
+      name: "",
+      process: "",
+      day: "",
+      telephone: "",
+      email: "",
+      website: "",
+      service: "",
+      category: []
+    })
   }
 
   updateNameField = event => {
