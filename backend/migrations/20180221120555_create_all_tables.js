75c2b2723d9a757d575d0a744cec64c12c10f280
yevhensydorov <assid@ukr.net>
2018-03-12 22:33:00 +0000

    add users table to migrations

--- backend/migrations/20180221120555_create_all_tables.js
+++ backend/migrations/20180221120555_create_all_tables.js
@@ -56,6 +56,12 @@ exports.up = knex =>
         .inTable('Address');
       table.string('lat');
       table.string('long');
+    })
+    .createTable('Users', (table) => {
+      table.increments('user_id').primary();
+      table.string('username');
+      table.string('salt_password');
+      table.date('last_updated');
     });
 
 exports.down = knex =>
