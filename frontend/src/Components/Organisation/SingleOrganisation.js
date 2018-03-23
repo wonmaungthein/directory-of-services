51216d86b8636985c13d1cd02f3851b3ef10b99b
louiaR <louitiani@gmail.com>
2018-03-23 09:20:47 +0000

    fix website hyperlink

--- frontend/src/Components/Organisation/SingleOrganisation.js
+++ frontend/src/Components/Organisation/SingleOrganisation.js
@@ -3,7 +3,7 @@ import Dialog, {
   DialogTitle,
   DialogContent,
   DialogActions,
-  withMobileDialog
+  withMobileDialog,
 } from 'material-ui/Dialog';
 import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
@@ -16,17 +16,16 @@ class SingleOrganisation extends Component {
     editIdx: -1,
   };
 
-  editSelectedOrganisation = (idex) => (
+  editSelectedOrganisation = idex =>
     this.setState({
       editIdx: idex,
       open: true,
-    })
-  );
+    });
 
   stopEditing = () => {
     this.setState({
       editIdx: -1,
-      open: false
+      open: false,
     });
   };
 
@@ -39,88 +38,96 @@ class SingleOrganisation extends Component {
   };
   render() {
     const data = this.props.editOrgData;
-     const { fullScreen } = this.props;
+    const { fullScreen } = this.props;
     const index = 1;
     const { editIdx } = this.state;
     const currentlyEditing = editIdx === index;
 
-    return currentlyEditing ? <EditOrganisation editOrgData={data} stopEditing={this.stopEditing} show /> :
-      (
-        <div className="single-oganisation">
-          <Button
-            onClick={this.editSelectedOrganisation}
-            variant="raised"
-            size="small"
-            className="orgnaization-detais-button button-title"
-          >
-            <i className="material-icons">add</i>DETAILS
-          </Button>
-          <Dialog
-            fullScreen={fullScreen}
-            className="single-org-dialog"
-            open={this.state.open}
-            onClose={this.handleClose}
-          >
-            <DialogActions>
-              <button
-                onClick={this.handleClose}
-                className="single-oganisation-close-button"
-              >
-                <i className="material-icons">close</i>
-              </button>
-            </DialogActions>
-            <EditOrganisation getData={() => this.editSelectedOrganisation(index)} />
-            <DialogTitle
-              className="single-oganisation-title"
-              id="alert-dialog-title"
+    return currentlyEditing ? (
+      <EditOrganisation
+        editOrgData={data}
+        stopEditing={this.stopEditing}
+        show
+      />
+    ) : (
+      <div className="single-oganisation">
+        <Button
+          onClick={this.editSelectedOrganisation}
+          variant="raised"
+          size="small"
+          className="orgnaization-detais-button button-title"
+        >
+          <i className="material-icons">add</i>DETAILS
+        </Button>
+        <Dialog
+          fullScreen={fullScreen}
+          className="single-org-dialog"
+          open={this.state.open}
+          onClose={this.handleClose}
+        >
+          <DialogActions>
+            <button
+              onClick={this.handleClose}
+              className="single-oganisation-close-button"
             >
-              {data.Organisation}
-            </DialogTitle>
-            <DialogContent className="single-oganisation-content">
-              <h4 className="details-area">Area: {data.Area} | Borough: {data.Borough}</h4>
-              <div className="health-advice-process">
-                <h4> Services</h4>
-                <p className="service"> {data.Services}</p>
+              <i className="material-icons">close</i>
+            </button>
+          </DialogActions>
+          <EditOrganisation
+            getData={() => this.editSelectedOrganisation(index)}
+          />
+          <DialogTitle
+            className="single-oganisation-title"
+            id="alert-dialog-title"
+          >
+            {data.Organisation}
+          </DialogTitle>
+          <DialogContent className="single-oganisation-content">
+            <h4 className="details-area">
+              Area: {data.Area} | Borough: {data.Borough}
+            </h4>
+            <div className="health-advice-process">
+              <h4> Services</h4>
+              <p className="service"> {data.Services}</p>
+            </div>
+            <div className="single-process-date">
+              <div>
+                <h4>Process</h4>
+                <p className="service">{data.Process}</p>
               </div>
-              <div className="single-process-date">
-                <div>
-                  <h4>Process</h4>
-                  <p className="service">{data.Process}</p>
-                </div>
-                <div>
-                  <h4>Days</h4>
-                  <p>{data.Day}</p>
-                </div>
+              <div>
+                <h4>Days</h4>
+                <p>{data.Day}</p>
               </div>
-              <div className="single-telephone-email">
-                <div>
-                  <h4>Telephone</h4>
-                  <p>{data.Tel}</p>
-                </div>
-                <div>
-                  <h4>Email</h4>
-                  <p>{data.Email}</p>
-                </div>
+            </div>
+            <div className="single-telephone-email">
+              <div>
+                <h4>Telephone</h4>
+                <p>{data.Tel}</p>
               </div>
-              <a
-                className="website-link"
-                target="blank"
-                href={`${data.Website}`}
-              >
-                Website
-              </a>
-              <h5 className="detail-footer">
-                {data.Category}
-              </h5>
-            </DialogContent>
-            <DialogActions> 
-              <Button variant="fab" className="exit" onClick={this.handleClose} color="primary">
-                <i className="material-icons">exit_to_app</i>
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </div>
-      );
+              <div>
+                <h4>Email</h4>
+                <p>{data.Email}</p>
+              </div>
+            </div>
+            <h4>Website</h4>
+            <a className="website-link" target="blank" href={`${data.Website}`}>
+              <h5 className="">{data.Website}</h5>
+            </a>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="fab"
+              className="exit"
+              onClick={this.handleClose}
+              color="primary"
+            >
+              <i className="material-icons">exit_to_app</i>
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
   }
 }
 
