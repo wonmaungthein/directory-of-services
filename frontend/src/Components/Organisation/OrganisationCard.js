41a120e4eec74c89540b4a48fcc222cdabf38126
MrDwina <diwina1993@hotmail.com>
2018-03-30 20:21:41 +0100

    moved Delete button to be editt button

--- frontend/src/Components/Organisation/OrganisationCard.js
+++ frontend/src/Components/Organisation/OrganisationCard.js
@@ -1,33 +1,26 @@
 import React from 'react';
 import Paper from 'material-ui/Paper';
 import SingleOrganisation from './SingleOrganisation';
-import Notification from '../Users/Notification';
+import EditOrganisation from './EditOrganisation';
 import './index.css';
 
 const OrganisationCard = ({
   org,
-  Organisation,
-  Area,
-  Services,
-  Process,
-  deleteOrganisation,
+  getData,
 }) => (
   <Paper className="form-card">
+    <EditOrganisation getData={getData} />    
     <SingleOrganisation editOrgData={org} />
     <div className="single-orgonaization-details">
-      <h4>{Organisation}</h4>
+      <h4>{org.Organisation}</h4>
       <h6>
-        Area: {Area} | Borough: {org.Borough}
+        Area: {org.Area} | Borough: {org.Borough}
       </h6>
       <div className="health-advice-process">
-        <p> - {Services}</p>
-        <p> - {Process}</p>
+        <p> - {org.Services}</p>
+        <p> - {org.Process}</p>
       </div>
     </div>
-    <Notification
-      organisation={Organisation}
-      removeHandler={deleteOrganisation}
-    />
   </Paper>
 );
 
