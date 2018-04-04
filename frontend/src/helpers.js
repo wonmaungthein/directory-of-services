d13cc367d77949a73fa7697561b7a5f9d337b60f
MrDwina <diwina1993@hotmail.com>
2018-04-04 13:04:21 +0100

    edited categoryNameMaker function name

--- frontend/src/helpers.js
+++ frontend/src/helpers.js
@@ -145,7 +145,7 @@ function reformatCategoryName(name) {
 
 function categoryNameMaker(name) {
   return name
-    ? name.replace('/', '').replace('/', '').replace("services/", '').replace("/add", '').replace(/\b\w/g, l => l.toUpperCase())
+    ? name.replace('/', '').replace("services/", '').replace("/add", '').replace(/\b\w/g, l => l.toUpperCase())
     : null;
 }
 
