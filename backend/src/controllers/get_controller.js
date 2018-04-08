5c52997ff4a7fb18de5cbba99375ff4ab20a97c9
MrDwina <diwina1993@hotmail.com>
2018-04-08 17:38:48 +0100

    created list of categories names, boroughs names and areas names API

--- backend/src/controllers/get_controller.js
+++ backend/src/controllers/get_controller.js
@@ -1,17 +1,22 @@
-
-import { Model } from 'objection';
-import { knex } from '../../config';
 import Organisation from '../model/Organisation';
-
-Model.knex(knex);
-
-const getAllOrgainisation = async () => {
-  const result = await Organisation
-    .query()
-    .eager('[branch, branch.[address, address.[location] service, service.[categories]] ]');
-  return result;
-}
+import Categories from '../model/Categories';
+import Branch from '../model/Branch';
+import Address from '../model/Address';
 
 module.exports = {
-  getAllOrgainisation
+  getAllOrgainisation: async () => {
+    const result = await Organisation
+      .query()
+      .eager('[branch, branch.[address, address.[location] service, service.[categories]] ]');
+    return result;
+  },
+
+  getListOfCategoriesName: () =>
+    Categories.query().skipUndefined(),
+
+  getListOfBoroughsNames: () =>
+    Branch.query().skipUndefined(),
+
+  getListOfAreasNames: () =>
+    Address.query().skipUndefined()
 };
--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -1,7 +1,13 @@
 
 import { postOrganisation } from '../controllers/post_controller';
-import { getAllOrgainisation } from '../controllers/get_controller';
 import { loadSeedToDb } from '../controllers/postInitialData';
+import { removeDuplications } from '../helpers';
+import {
+  getAllOrgainisation,
+  getListOfCategoriesName,
+  getListOfBoroughsNames,
+  getListOfAreasNames
+} from '../controllers/get_controller';
 
 module.exports = router => {
   router.post('/service', (req, res) => {
@@ -12,6 +18,18 @@ module.exports = router => {
     getAllOrgainisation().then(data => res.json(data));
   });
   router.get('/service/migrate', (req, res) => {
-    loadSeedToDb().then(data => res.json({ Migration: 'Data migration to database completed successfully !' }));
-  })
+    loadSeedToDb().then(() => res.json({ Migration: 'Data migration to database completed successfully !' }));
+  });
+
+  router.get('/service/categories', (req, res) =>
+    getListOfCategoriesName().map(data => data.cat_name)
+      .then(categories => res.json(removeDuplications(categories))));
+
+  router.get('/service/boroughs', (req, res) =>
+    getListOfBoroughsNames().map(data => data.borough)
+      .then(boroughs => res.json(removeDuplications(boroughs))));
+
+  router.get('/service/areas', (req, res) =>
+    getListOfAreasNames().map(data => data.area)
+      .then(areas => res.json(removeDuplications(areas))));
 }
