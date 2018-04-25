fe039df939c4ab72bc05d54f0a031ccc1cfce1bc
louiaR <louitiani@gmail.com>
2018-04-25 20:50:25 +0100

    Add initial state, handlechange function

--- frontend/src/Components/Users/forgot-password/index.js
+++ frontend/src/Components/Users/forgot-password/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Grid from 'material-ui/Grid';
 import Button from 'material-ui/Button';
 import Paper from 'material-ui/Paper';
@@ -6,28 +6,56 @@ import TextField from 'material-ui/TextField';
 
 import './forgotPassword.css';
 
-const HomeSearch = props => (
-  <div className="forgot-password">
-    <Paper className="form-card">
-      <p className="forgot-info">
-        Enter your email address we will sent you the password reset link.
-      </p>
-      <Grid container spacing={24}>
-        <form className="forgot-password-form">
-          <TextField placeholder="Email" id="email" required />
-          <Button
-            variant="raised"
-            size="small"
-            type="submit"
-            className="add-user-button"
-            onSubmit={props.handleSubmit}
-          >
-            Sent
-          </Button>
-        </form>
-      </Grid>
-    </Paper>
-  </div>
-);
+class ForgotPassword extends Component {
+  state = {
+    email: '',
+  };
 
-export default HomeSearch;
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      email: '',
+    });
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div className="forgot-password">
+        <Paper className="form-card">
+          <h4>Forgot Your Forgot Password</h4>
+          <p className="forgot-info">
+            Enter your email address we will sent you the password reset link.
+          </p>
+          <Grid container spacing={24} onSubmit={this.handleSubmit}>
+            <form className="forgot-password-form">
+              <TextField
+                placeholder="Email"
+                id="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                required
+              />
+              <Button
+                variant="raised"
+                size="small"
+                type="submit"
+                className="add-user-button"
+              >
+                Sent
+              </Button>
+            </form>
+          </Grid>
+        </Paper>
+      </div>
+    );
+  }
+}
+
+export default ForgotPassword;
