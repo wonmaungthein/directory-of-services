0b3a3c8789bda39312ffbd3dedb3db9579c30d56
louiaR <louitiani@gmail.com>
2018-04-25 18:23:41 +0100

    add paper from material

--- frontend/src/Components/Users/forgot-password/index.js
+++ frontend/src/Components/Users/forgot-password/index.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import Grid from 'material-ui/Grid';
 import Button from 'material-ui/Button';
+import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 
 import './forgotPassword.css';
 
-const HomeSearch = () => (
+const HomeSearch = props => (
   <div className="forgot-password">
-    <p className="forgot-info">
-      Enter your email address we will sent you the password reset link.
-    </p>
-    <Grid container spacing={24}>
-      <form className="forgot-password-form">
-        <TextField placeholder="Email" id="email" required />
-        <Button
-          variant="raised"
-          size="small"
-          type="submit"
-          className="add-user-button"
-          // onClick={props.handleSubmit}
-        >
-          Sent
-        </Button>
-      </form>
-    </Grid>
+    <Paper className="form-card">
+      <p className="forgot-info">
+        Enter your email address we will sent you the password reset link.
+      </p>
+      <Grid container spacing={24}>
+        <form className="forgot-password-form">
+          <TextField placeholder="Email" id="email" required />
+          <Button
+            variant="raised"
+            size="small"
+            type="submit"
+            className="add-user-button"
+            onSubmit={props.handleSubmit}
+          >
+            Sent
+          </Button>
+        </form>
+      </Grid>
+    </Paper>
   </div>
 );
 
