338011b9a5261f0c9726b4d140892462dc465ebd
louiaR <louitiani@gmail.com>
2018-04-26 15:49:03 +0100

    import forgot password component temporary

--- frontend/src/Components/LandingPage/index.js
+++ frontend/src/Components/LandingPage/index.js
@@ -2,22 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import LoginForm from '../LoginForm';
-import ForgotPassword from '../Users/forgot-password'
+// import ForgotPassword from '../Users/forgot-password';  waiting Mohammad change to define route for this component
 import { deleteFlashMessage } from '../../actions/flashMessages';
 import './landing-page.css';
 
-const LandingPage = (props) => {
+const LandingPage = props => {
+  const renderMessages = () =>
+    props.messages
+      .map(message => (message.type === 'loginError' ? message.text : null))
+      .join('');
 
-  const renderMessages = () => (
-    props.messages.map(message => message.type === 'loginError' ? message.text : null).join('')
-  );
-
-  const deleteMessage = () => (
-    props.messages.map(message => message.type === 'loginError' ? props.deleteFlashMessage(message.id) : null)
-  );
+  const deleteMessage = () =>
+    props.messages.map(
+      message =>
+        message.type === 'loginError'
+          ? props.deleteFlashMessage(message.id)
+          : null,
+    );
 
   const showRrrorMessage = () => {
-    const errorMessage = renderMessages() ? <h3 className="login-error">{renderMessages()} <i tabIndex={0} role="button" onClick={deleteMessage} onKeyPress={deleteMessage} className="material-icons">close</i></h3> : null;
+    const errorMessage = renderMessages() ? (
+      <h3 className="login-error">
+        {renderMessages()}{' '}
+        <i
+          tabIndex={0}
+          role="button"
+          onClick={deleteMessage}
+          onKeyPress={deleteMessage}
+          className="material-icons"
+        >
+          close
+        </i>
+      </h3>
+    ) : null;
     return errorMessage;
   };
 
@@ -25,23 +42,22 @@ const LandingPage = (props) => {
     <div className="landing-page">
       <div>
         {showRrrorMessage()}
-        
-        <ForgotPassword />
+        <LoginForm />
+        {/* <ForgotPassword /> */}
       </div>
     </div>
-  )
-
-}
+  );
+};
 
 function mapStateToProps(state) {
   return {
-    messages: state.flashMessages
-  }
+    messages: state.flashMessages,
+  };
 }
 
 LandingPage.propTypes = {
   messages: PropTypes.array.isRequired,
-  deleteFlashMessage: PropTypes.func.isRequired
-}
+  deleteFlashMessage: PropTypes.func.isRequired,
+};
 
 export default connect(mapStateToProps, { deleteFlashMessage })(LandingPage);
