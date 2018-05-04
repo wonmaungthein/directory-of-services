b5a3d2a1aea00c0fa992be1ecbfa367a27c13b2b
louiaR <louitiani@gmail.com>
2018-05-04 13:08:15 +0100

    Set redux action

--- frontend/src/actions/getApiData.js
+++ frontend/src/actions/getApiData.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { LIST_OF_CATEGORIES, LIST_OF_BOROUGHS, LIST_OF_AREAS, FILTERED_BRANCHS_BY_CATEGORY } from './types';
+import { LIST_OF_CATEGORIES, LIST_OF_BOROUGHS, LIST_OF_AREAS, FILTERED_BRANCHS_BY_CATEGORY,  LIST_OF_ORGANISATIONS } from './types';
 
 const api = 'http://localhost:3002/api' || `${process.env.API_URL}/api`;
 
@@ -58,3 +58,18 @@ export function getBranchsByCategory(category) {
       .then(branchs => dispatch(setGetBranchsByCategory(branchs.data)))
   }
 }
+
+export function setOrganisationsList(organisations) {
+  return{
+    type: LIST_OF_ORGANISATIONS,
+    organisations
+  }
+}
+
+export function getOrganisationsList(){
+  return dispatch => {
+    axios.get(`${api}/service/all`)
+    .then(all => dispatch(setOrganisationsList(all)))
+  }
+}
+
