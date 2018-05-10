d384fe7004e8da91c35971ba06800d150973d88f
Kash <kash.karimi@gmail.com>
2018-05-10 16:00:56 +0100

    fix circle

--- .circleci/deploy_frontend.sh
+++ .circleci/deploy_frontend.sh
@@ -1,11 +1,17 @@
-AWS_DEFAULT_REGION=eu-west-1 \
-AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+export AWS_DEFAULT_REGION=eu-west-1
+export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+export CLOUDFRONT_DISTRO_ID=E1YRVRN38DJTEK
+export BUCKET=cyf-dos
 
-aws s3 sync frontend/build/ s3://cyf-dos --delete --acl public-read
+sudo apt-get -y -qq install awscli
+aws configure set preview.cloudfront true
 
 # invalidate cache in cloudfront
-aws configure set preview.cloudfront true
+TIMESTAMP=$(date '+%Y-%m-%d-%H-%M-%S')
+sed -i -e "s/__TIMESTAMP__/${TIMESTAMP}/g" .circleci/invalidation-batch.json
+
+aws s3 sync frontend/build/ s3://${BUCKET} --acl public-read --delete --cache-control max-age=3600
 aws cloudfront create-invalidation \
-    --distribution-id E1YRVRN38DJTEK \
-    --paths "/*"
+    --distribution-id $CLOUDFRONT_DISTRO_ID \
+    --invalidation-batch file://.circleci/invalidation-batch.json
