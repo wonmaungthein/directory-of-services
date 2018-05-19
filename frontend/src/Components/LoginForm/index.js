cbf9614b5a7bc46a08e39feafddea6dd6a91f187
coderyourfuture <you@example.com>
2018-05-19 17:11:44 +0100

    Spinner component added to LoginForm

--- frontend/src/Components/LoginForm/index.js
+++ frontend/src/Components/LoginForm/index.js
@@ -7,7 +7,9 @@ import Paper from 'material-ui/Paper';
 import Button from 'material-ui/Button';
 import { connect } from 'react-redux';
 import { login } from '../../actions/loginActions';
-import './login-form.css'
+import Spinner from '../Spinner';
+import './login-form.css';
+
 
 class LoginForm extends Component {
   state = {
@@ -15,47 +17,55 @@ class LoginForm extends Component {
     password: '',
     isLoading: false,
     errors: {},
-    userVerification: ''
-  }
+    userVerification: '',
+  };
+
 
   validation = () => {
     let isError = false;
     const errors = {
-      usernameErr: "",
-      passwordErr: "",
-    }
+      usernameErr: '',
+      passwordErr: '',
+    };
     if (this.state.username.length <= 0) {
       isError = true;
-      errors.usernameErr = "You have to add your username";
+      errors.usernameErr = 'You have to add your username';
     }
     if (this.state.password.length <= 0) {
       isError = true;
-      errors.passwordErr = "You have to add your password";
+      errors.passwordErr = 'You have to add your password';
     }
     this.setState({
       ...this.setState,
-      errors
-    })
+      errors,
+    });
     return isError;
-  }
+  };
 
-  handleFieldsChange = (e) => {
+
+  handleFieldsChange = e => {
     e.preventDefault();
     this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  handleLogin = (e) => {
+
+  handleLogin = e => {
     e.preventDefault();
     const errors = this.validation();
     if (!errors) {
       this.setState({ errors: {}, isLoading: true });
       this.props.login(this.state).then(user => {
         if (user.data && user.data.success !== false) {
-          this.context.router.history.push('/home')
+          this.setState({ isLoading: true });
+          this.context.router.history.push('/home');
+          this.setState({ isLoading: false });
         } else {
-          this.setState({ userVerification: user.data.message, isLoading: false })
+          this.setState({
+            userVerification: user.data.message,
+            isLoading: false,
+          });
         }
       });
       this.setState({
@@ -63,64 +73,75 @@ class LoginForm extends Component {
         isLoading: true,
         username: '',
         password: '',
-        userVerification: ''
+        userVerification: '',
       });
     }
-  }
+  };
 
-  handleBlur = (e) => {
+
+  handleBlur = e => {
     e.preventDefault();
-    this.setState({ userVerification: '' })
-  }
+    this.setState({ userVerification: '' });
+  };
+
 
   render() {
     const { usernameErr, passwordErr } = this.state.errors;
-    return (
-      <Paper className="login-form-content">
-        <FormHelperText className="error">{this.state.userVerification}</FormHelperText>
-        <Typography color="primary" variant="display3">
-          Login
-        </Typography>
-        <form className="login-form">
-          <Fragment>
-            <TextField
-              id="username"
-              label="Username"
-              name="username"
-              value={this.state.username}
-              onChange={this.handleFieldsChange}
-              onBlur={this.handleBlur}
-              margin="normal"
-            />
-            <FormHelperText className="error">{usernameErr}</FormHelperText>
-          </Fragment>
-          <Fragment>
-            <TextField
-              id="password"
-              label="Password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleFieldsChange}
-              onBlur={this.handleBlur}
-              margin="normal"
-              type="password"
-            />
-            <FormHelperText className="error">{passwordErr}</FormHelperText>
-          </Fragment>
-          <Button onClick={this.handleLogin} variant="raised" color="primary">
-            Submit
-          </Button>
-        </form>
-      </Paper>
-    )
+    if (this.state.isLoading) {
+      return <Spinner />;
+    }
+      return (
+        <Paper className="login-form-content">
+          <FormHelperText className="error">
+            {this.state.userVerification}
+          </FormHelperText>
+          <Typography color="primary" variant="display3">
+            Login
+          </Typography>
+          <form className="login-form">
+            <Fragment>
+              <TextField
+                id="username"
+                label="Username"
+                name="username"
+                value={this.state.username}
+                onChange={this.handleFieldsChange}
+                onBlur={this.handleBlur}
+                margin="normal"
+              />
+              <FormHelperText className="error">{usernameErr}</FormHelperText>
+            </Fragment>
+            <Fragment>
+              <TextField
+                id="password"
+                label="Password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleFieldsChange}
+                onBlur={this.handleBlur}
+                margin="normal"
+                type="password"
+              />
+              <FormHelperText className="error">{passwordErr}</FormHelperText>
+            </Fragment>
+            <Button onClick={this.handleLogin} variant="raised" color="primary">
+              Submit
+            </Button>
+          </form>
+        </Paper>
+      );
+
+
   }
 }
 
+
 LoginForm.propTypes = {
-  login: PropTypes.func.isRequired
-}
+  login: PropTypes.func.isRequired,
+};
 LoginForm.contextTypes = {
   router: PropTypes.object.isRequired,
-}
+};
+
 
 export default connect(null, { login })(LoginForm);
--- frontend/src/Components/Spinner/index.js
+++ frontend/src/Components/Spinner/index.js
@@ -3,16 +3,25 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import { CircularProgress } from 'material-ui/Progress';
 import purple from 'material-ui/colors/purple';
+import './spinner.css';
 
 const styles = theme => ({
   progress: {
-    margin: theme.spacing.unit * 2,
+    margin: theme.spacing.unit * 6,
   },
 });
 
 function CircularIndeterminate(props) {
   const { classes } = props;
-  return (<CircularProgress className={classes.progress} style={{ color: purple[500] }} thickness={7} />
+  return (
+    <div className="spinner">
+      <CircularProgress
+        className={classes.progress}
+        style={{ color: purple[500] }}
+        thickness={6}
+        size={50}
+      />
+    </div>
   );
 }
 
