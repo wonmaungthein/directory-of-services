ad96faec194778635bec34b413b178b98726408f
Kash <kash.karimi@gmail.com>
2018-05-22 22:46:05 +0100

    add prod values

--- backend/config.js
+++ backend/config.js
@@ -1,10 +1,10 @@
 
-import Knex from 'knex';
-import knexFile from './knexFile';
+const knex = require('knex');
+const knexFile = require('./knexFile');
 
 const environment = process.env.NODE_ENV || 'development';
 module.exports = {
-  knex: Knex(knexFile[environment]),
+  knex: knex(knexFile[environment]),
   port: process.env.PORT || 3002,
   apiPrefic: 'api'
 }
