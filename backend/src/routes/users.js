e2fbc6f200c976c4f30103d65142c84c5788ce1a
MrDwina <diwina1993@hotmail.com>
2018-06-09 11:16:52 +0100

    corrected sideHost to siteHost

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -202,7 +202,7 @@ router.post('/forgot', (req, res) => {
         subject: 'Reset User Password',
         text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
           Please click on the following link, or paste this into your browser to complete the process:\n\n
-          http://${req.body.sideHost}/reset/${token}\n\n
+          http://${req.body.siteHost}/reset/${token}\n\n
           If you did not request this, please ignore this email and your password will remain unchanged.\n`
       })
 
