19a87a53385d7e7d236f2d5d07c374db178a11af
MrDwina <diwina1993@hotmail.com>
2018-06-11 12:27:40 +0100

    fixed ci error

--- backend/library/helpers.js
+++ backend/library/helpers.js
@@ -26,12 +26,11 @@ function round(value, place) {
 // Calculate the distance
 function calculateDistance(lat1, lon1, lat2, lon2) {
   const radius = 6371; // Radius of the earth in km
-  const dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  const dLat = deg2rad(lat2 - lat1); // deg2rad below
   const dLon = deg2rad(lon2 - lon1);
-  const a =
-    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-    Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const firtsValue = (Math.sin(dLat / 2) * Math.sin(dLat / 2));
+  const secondValue = (Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))) * (Math.sin(dLon / 2) * Math.sin(dLon / 2));
+  const a = firtsValue + secondValue;
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const distance = radius * c; // Distance in km
   return round(distance, 3);
