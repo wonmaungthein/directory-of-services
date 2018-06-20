bcf1157eddac9de1f42c8cca7d739bf114213926
MrDwina <diwina1993@hotmail.com>
2018-06-20 21:08:50 +0100

    used try and catch instead of then with axios

--- frontend/src/actions/postData.js
+++ frontend/src/actions/postData.js
@@ -3,48 +3,73 @@ import axios from 'axios';
 const api = process.env.REACT_APP_API_URL || process.env.REACT_APP_LOCALHOST_API_URL;
 
 export function addOrganisation(data) {
-  const saveOrganisation = () =>
-    axios.post(`${api}/service/organisation/add`, data).then(res => res);
+  const saveOrganisation = async () => {
+    try {
+      const res = await axios.post(`${api}/service/organisation/add`, data);
+      return res;
+    } catch (error) {
+      return JSON.stringify(error);
+    }
+  }
   return saveOrganisation
 }
 
 export function editOrganisation(data) {
-  const saveOrganisation = () =>
-    axios.patch(`${api}/service/organisation/edit`, data).then(res => res);
+  const saveOrganisation = async () => {
+    try {
+      const res = await axios.patch(`${api}/service/organisation/edit`, data);
+      return res;
+    } catch (error) {
+      return JSON.stringify(error);
+    }
+  }
   return saveOrganisation
 }
 
 export function requestRestPassword(data) {
-  const requestFun = () =>
-    axios.post(`${api}/forgot`, data)
-      .then(response => response)
-      .catch(error => error.response);
+  const requestFun = async () => {
+    try {
+      const res = await axios.post(`${api}/forgot`, data)
+        .catch(err => err.response)
+      return res;
+    } catch (error) {
+      return JSON.stringify(error)
+    }
+  }
   return requestFun
 }
 
 export function requestChangePassword(data) {
-  const requestFun = () =>
-    axios.post(`${api}/reset/:token`, data)
-      .then(response => response)
-      .catch(error => error.response);
+  const requestFun = async () => {
+    try {
+      const res = await axios.post(`${api}/reset/:token`, data);
+      return res;
+    } catch (error) {
+      return JSON.stringify(error);
+    }
+  }
   return requestFun
 }
 
 export function validateToken(token) {
-  const requestFun = () =>
-    axios.get(`${api}/reset/${token}`)
-      .then(response => response)
-      .catch(error => error.response);
+  const requestFun = async () => {
+    try {
+      const res = await axios.get(`${api}/reset/${token}`);
+      return res;
+    } catch (error) {
+      return JSON.stringify(error);
+    }
+  }
   return requestFun
 }
 
 export function getBranchesFilteredByPostCode(data) {
   const sendInfo = async () => {
     try {
-      const res = await axios.post(`${api}/service/postcode`, data)
+      const res = await axios.post(`${api}/service/postcode`, data);
       return res;
-    } catch (err) {
-      return err;
+    } catch (error) {
+      return JSON.stringify(error);
     }
   }
   return sendInfo
