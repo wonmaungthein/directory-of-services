ea2c2250c3298d64b085a5fc4001bafd645cce2c
louiaR <louitiani@gmail.com>
2018-06-22 12:12:51 +0100

    Customisation of home search

--- frontend/src/Components/HomePage/HomeSearch.js
+++ frontend/src/Components/HomePage/HomeSearch.js
@@ -1,8 +1,8 @@
-import React, { Fragment } from 'react';
+import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
 import Autosuggest from 'react-autosuggest';
-import { withStyles } from 'material-ui/styles';
-import { withRouter } from 'react-router-dom';
+import {withStyles} from 'material-ui/styles';
+import {withRouter} from 'react-router-dom';
 import Button from 'material-ui/Button';
 import Grid from 'material-ui/Grid';
 import EditOrganisation from '../Organisation/EditOrganisation';
@@ -15,23 +15,23 @@ import './HomePage.css'
 const styles = theme => ({
   container: {
     flexGrow: 1,
-    position: 'relative',
+    position: 'relative'
   },
   suggestionsContainerOpen: {
     position: 'absolute',
     zIndex: 1,
     marginTop: theme.spacing.unit,
     left: 0,
-    right: 0,
+    right: 0
   },
   suggestion: {
-    display: 'block',
+    display: 'block'
   },
   suggestionsList: {
     margin: 0,
     padding: 0,
-    listStyleType: 'none',
-  },
+    listStyleType: 'none'
+  }
 });
 
 class HomeSearch extends React.Component {
@@ -40,67 +40,76 @@ class HomeSearch extends React.Component {
     suggestions: [],
     organisations: [],
     editIdx: -1,
-    search: ''
+    search: '',
+    isHidden: true,
   };
 
   componentWillReceiveProps(newProps) {
-    const organisations = newProps.organisations ? newProps.organisations.data : [];
-    this.setState({
-      organisations: organisations
-    });
+    const organisations = newProps.organisations
+      ? newProps.organisations.data
+      : [];
+    this.setState({organisations: organisations});
   }
 
-  handleSuggestionsFetchRequested = ({ value }) => {
+  // allow user to press enter key for search
+  handleKeyUp = (e) => {
+    if (e.charCode === 13 || e.key === 'Enter') {
+      this.updateSearchData();
+    }
+  }
+
+  handleSuggestionsFetchRequested = ({value}) => {
     this.setState({
-      suggestions: helpers.getMainSearchSuggestions(value, this.state.organisations),
+      suggestions: helpers.getMainSearchSuggestions(value, this.state.organisations)
     });
   };
 
   handleSuggestionsClearRequested = () => {
-    this.setState({
-      suggestions: [],
-    });
+    this.setState({suggestions: []});
   };
 
-  handleChange = (event, { newValue }) => {
-    this.setState({
-      value: newValue,
-    });
+  handleChange = (event, {newValue}) => {
+    this.setState({value: newValue, isHidden: false,});
   };
 
-  editSelectedOrganisation = idex =>
-    this.setState({
-      editIdx: idex,
-    });
+  editSelectedOrganisation = idex => this.setState({editIdx: idex});
 
   stopEditing = () => {
-    this.setState({
-      editIdx: -1,
-    });
+    this.setState({editIdx: -1});
   };
 
   updateSearchData = () => {
-    this.setState({ search: this.state.value })
+    this.setState({search: this.state.value, isHidden: true, })
   }
 
+  // clear input value 
   clearSearchField = () => {
-    this.setState({ search: '', value: '' })
+    this.setState({ value: '', isHidden: true,})
   }
 
   filterData = (orgs) => {
-    const { search } = this.state;
+    const {search} = this.state;
     if (search) {
-      return orgs.filter(org =>
-        org.org_name.toLowerCase().includes(search.toLowerCase()) ||
-        org.borough.toLowerCase().includes(search.toLowerCase())
-      ).sort(helpers.sortArrObj)
+      return orgs.filter(org => org.org_name.toLowerCase().includes(search.toLowerCase()) || org.borough.toLowerCase().includes(search.toLowerCase())
+       // return branches which service match or part of search 
+      || search.toLowerCase().includes(org.service.toLowerCase()) 
+      // return all branches which provide service on that day
+      || org.service_days.toLowerCase().includes(search.toLowerCase())
+      // return all branches if search match cat_name
+      || org.cat_name.toLowerCase().includes(search.toLowerCase())
+      // return all branches if search tag match 
+      || org.tag.toLowerCase().includes(search.toLowerCase())
+      // return all branches if search tag match
+      || org.area.toLowerCase().includes(search.toLowerCase())
+      )
+      .sort(helpers.sortArrObj)
     }
     return [];
   }
 
   render() {
-    const { editIdx, organisations } = this.state;
-    const { classes } = this.props;
+    const {editIdx, organisations} = this.state;
+    const {classes} = this.props;
     if (organisations.length <= 0) {
       return <Spinner />
     }
@@ -109,70 +118,76 @@ class HomeSearch extends React.Component {
         <div>
           <Grid container className="organisation-page" spacing={24}>
             <Grid item xs={12} className="search-input">
-              <Autosuggest
-                theme={{
+              <div className="search-text">
+                <Autosuggest
+                  theme={{
                   container: classes.container,
                   suggestionsContainerOpen: classes.suggestionsContainerOpen,
                   suggestionsList: classes.suggestionsList,
-                  suggestion: classes.suggestion,
+                  suggestion: classes.suggestion
                 }}
-                renderInputComponent={helpers.renderInput}
-                suggestions={this.state.suggestions}
-                onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
-                onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
-                renderSuggestionsContainer={helpers.renderSuggestionsContainer}
-                getSuggestionValue={helpers.getMainSearchSuggestionValue}
-                renderSuggestion={helpers.renderMainSearchSuggestion}
-                inputProps={{
+                  renderInputComponent={helpers.renderInput}
+                  suggestions={this.state.suggestions}
+                  onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
+                  onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
+                  renderSuggestionsContainer={helpers.renderSuggestionsContainer}
+                  getSuggestionValue={helpers.getMainSearchSuggestionValue}
+                  renderSuggestion={helpers.renderMainSearchSuggestion}
+                  inputProps={{
                   classes,
                   placeholder: 'Search',
                   value: this.state.value,
                   onChange: this.handleChange,
-                }}
-              />
+                  onKeyUp: this.handleKeyUp
+                }} 
+                />
+                <button
+                  variant="raised"
+                  size="small"
+                  color="secondary"
+                  className={this.state.isHidden ? 'hidden' : 'clear-search'}
+                  onClick={this.clearSearchField}
+                >
+                  <i
+                    className="material-icons"
+                    size="small"
+                    variant="raised"
+                  >
+                    close
+                  </i>
+                </button>
+              </div>
               <Button
                 onClick={this.updateSearchData}
                 variant="raised"
                 color="primary"
                 size="small"
+                className="btn-search"
               >
                 Search
               </Button>
-              <Button
-                onClick={this.clearSearchField}
-                variant="raised"
-                size="small"
-                className="clear-search"
-                color="secondary"
-              >
-                Clear Search
-              </Button>
             </Grid>
-            {this.filterData(organisations).map((org, index) => {
-              const currentlyEditing = editIdx === index;
-              return currentlyEditing ? (
-                <Fragment>
-                  <EditOrganisation
-                    stopEditing={this.stopEditing}
-                    editOrgData={org}
-                    show
-                  />
-                  <SingleOrganisation
-                    stopEditing={this.stopEditing}
-                    handleShawDetails
-                    org={org}
-                  />
-                </Fragment>
-              ) : (
-                <Grid item xs={12} sm={6} key={org.id}>
-                  <OrganisationCard
-                    getData={() => this.editSelectedOrganisation(index)}
-                    org={org}
-                    index={index}
-                  />
-                </Grid>
-                );
-            })}
+            {this
+              .filterData(organisations)
+              .map((org, index) => {
+                const currentlyEditing = editIdx === index;
+                return currentlyEditing
+                  ? (
+                    <Fragment>
+                      <EditOrganisation stopEditing={this.stopEditing} editOrgData={org} show />
+                      <SingleOrganisation stopEditing={this.stopEditing} handleShawDetails org={org} />
+                    </Fragment>
+                  )
+                  : (
+                    <Grid item xs={12} sm={6} key={org.id}>
+                      <OrganisationCard
+                        getData={() => this.editSelectedOrganisation(index)}
+                        org={org}
+                        index={index}
+                      />
+                    </Grid>
+                  );
+              })}
           </Grid>
         </div >
       );
@@ -182,7 +197,17 @@ class HomeSearch extends React.Component {
 }
 
 HomeSearch.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(withRouter(props => <HomeSearch {...props} />));
+
+
+
+
+
+
+
+
+
+
