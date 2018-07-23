9305952e0bdc54a63c12d3b2dfea75538bb5ab8d
dwinatech <diwina1993@hotmail.com>
2018-07-23 18:10:23 +0100

    Created initial test to Categories, HomePage, HomeSearch, LandingPage, LoginForm and Login components

---
+++ frontend/src/_test_/_home_page_/Home_search.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducerRoot from '../../reducers';
+import HomeSearch from '../../Components/HomePage/HomeSearch';
+
+configure({ adapter: new Adapter() });
+
+const store = createStore(reducerRoot);
+
+describe('HomeSearch Component', () => {
+    it('Should render without crashing', () => {
+        const wrapper = 
+        shallow(<Provider store={store}><HomeSearch /></Provider>).length
+        expect(wrapper).toEqual(1)
+    })
+}) 
