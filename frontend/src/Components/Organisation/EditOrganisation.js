9239bb74b5a0351712c1cb2785a46bf61c72af14
louiaR <louitiani@gmail.com>
2018-07-23 16:10:06 +0100

    Update checkbox function

--- frontend/src/Components/Organisation/EditOrganisation.js
+++ frontend/src/Components/Organisation/EditOrganisation.js
@@ -15,29 +15,36 @@ const composedCategories = [
   "Gender Based Violence",
   "Mental Health Services",
   "Social and Other",
-  "Baby Equipment",
-  "Young People and Children"
+  "Baby Equipment"
 ]; 
 
-// Words that do not match any category's name
-const words = [
+const nonComposedCategories = [
   "Debt",
   "Trafficking",
-  "Destitution",
   "LGBTQI",
   "Healthcare",
   "Education",
   "Benefits",
-  "Employment",
+  "Families",
+  "Housing",
+  "Immigration",
+];
+
+// Categries format in FE
+const checkCategories = [
+  "Debt",
+  "Trafficking",
+  "LGBTQI",
+  "Healthcare",
+  "Education",
+  "Benefits",
   "Families",
   "Gender Based Violence",
   "Housing",
   "Immigration",
   "Mental Health Services",
   "Social and Other",
-  "Women",
   "Baby Equipment",
-  "Young People and Children"
 ];
 
 class EditOrganisation extends React.Component {
@@ -72,14 +79,24 @@ class EditOrganisation extends React.Component {
       const categories = [];
       
       // Data.cat_name.length is 1 => all category represents a single item 
-      // So I made a new array where each category is an individual item of the array
-      for(let i = 0; i < words.length; i += 1){
-        const index = data.cat_name.includes(words[i]);
+      // So made a new array where each category is an individual item of the array
+      for(let i = 0; i < checkCategories.length; i += 1){
+        const index = data.cat_name.includes(checkCategories[i]);
         if(index){
-          categories.push(words[i])
-        }
+          categories.push(checkCategories[i])
+        } 
       }
 
+      if (data.cat_name.includes('Pregnant Women and New Mothers')) {
+        categories.push('Women')
+      } else if (data.cat_name.includes("Employment/Training/Volunteering")){
+        categories.push('Employment');  
+      } else if (data.cat_name.includes("Young People/Children") || data.cat_name.includes("Young People and Children")){
+        categories.push('Young People and Children')        
+      } else if (data.cat_name.includes("Destitution/NRPF")){
+        categories.push('Destitution')
+      }
+      
       this.setState({
         branchId: data.branch_id,
         orgId: data.org_id,
@@ -129,8 +146,6 @@ class EditOrganisation extends React.Component {
     e.preventDefault();
     const days = this.state.Day.join(' ');
     const categories = this.state.Categories.join(' ');
-    console.log(categories, 'sub');
-    
     const orgData = {
       branchId: this.state.branchId,
       serviceId: this.state.serviceId,
@@ -179,17 +194,13 @@ class EditOrganisation extends React.Component {
     const listOfCategories = [...this.state.Categories];
     let index;
       for(let i = 0; i < composedCategories.length; i += 1) {
-        
         // Case where category is checked and category's name is composed
-          if(event.target.checked && event.target.value.includes(composedCategories[i].split(' ').join(''))) {
-            if(listOfCategories.indexOf(composedCategories[i]) === -1) {
+          if(event.target.checked && composedCategories[i].split(' ').join('').includes(event.target.value)) {
+            if(listOfCategories.indexOf(composedCategories[i]) === -1) { 
               listOfCategories.push(composedCategories[i]);              
-            } else if (listOfCategories.indexOf(composedCategories[i]) > -1){
-              index = listOfCategories.indexOf(event.target.value)
-              listOfCategories.splice(index, 1) 
-            }
+            } 
         } 
-
+        
         // Case where category is not checked and category's name is composed
         if(!event.target.checked && event.target.value.includes(composedCategories[i].split(' ').join(''))) { 
           if(listOfCategories.indexOf(composedCategories[i]) > -1) {
@@ -197,89 +208,118 @@ class EditOrganisation extends React.Component {
             listOfCategories.splice(index, 1)             
           }
         } 
+      }
 
+      // Special case 
+      // Young people
+      if ( event.target.checked && event.target.value.includes('Young People and Children'.split(' ').join(''))){
+        if(listOfCategories.indexOf('Young People and Children') === -1 ) {
+          
+          listOfCategories.push('Young People/Children')
+        } 
+        if(listOfCategories.indexOf('Young People and Children') > -1 ) {
+          index =  listOfCategories.indexOf('Young People and Children');
+          listOfCategories.splice(index, 1);
+          listOfCategories.push('Young People/Children')
+          
+        } 
+      }
+
+      if ( !event.target.checked && event.target.value.includes('Young People and Children'.split(' ').join(''))){
+        if(listOfCategories.includes('Young People and Children') || listOfCategories.includes('Young People/Children')) {        
+        index = listOfCategories.indexOf('Young People and Children') || listOfCategories.indexOf('Young People/Children');
+        listOfCategories.splice(index, 1);
+        } 
+      }  
+      
+      // Women
+      if ( event.target.checked && event.target.value.includes('Women')){
+        if(listOfCategories.indexOf('Women') === -1 ) {
+          
+          listOfCategories.push('Pregnant Women and New Mothers')
+        } 
+        if(listOfCategories.indexOf('Women') > -1 ) {
+          index =  listOfCategories.indexOf('Women');
+          listOfCategories.splice(index, 1);
+          listOfCategories.push('Pregnant Women and New Mothers')
+        } 
+      }
+
+      if ( !event.target.checked && event.target.value.includes('Women')){
+        if(listOfCategories.includes('Women') || listOfCategories.includes('Pregnant Women and New Mothers')) {        
+          index = listOfCategories.indexOf('Women') || listOfCategories.indexOf('Pregnant Women and New Mothers');
+          listOfCategories.splice(index, 1);
+          } else {
+            //
+          }
+      }  
+
+      // Employment
+      if ( event.target.checked && event.target.value === ('Employment')){
+        if(listOfCategories.indexOf('Employment') === -1 ) {
+          listOfCategories.push('Employment/Training/Volunteering')
+        } 
+        if(listOfCategories.indexOf('Employment') > -1 ) {
+          index =  listOfCategories.indexOf('Employment');
+          listOfCategories.splice(index, 1);
+          listOfCategories.push('Employment/Training/Volunteering')
+        } 
+      }
+      if ( !event.target.checked && event.target.value === ('Employment')){
+        if(listOfCategories.includes('Employment') || listOfCategories.includes('Employment/Training/Volunteering')) {        
+          index = listOfCategories.indexOf('Employment') || listOfCategories.indexOf('Employment/Training/Volunteering');
+          listOfCategories.splice(index, 1);
+          } else {
+            //
+          }
+      }
+
+      if ( event.target.checked && event.target.value === ('Destitution')){
+        if(listOfCategories.indexOf("Destitution/NRPF") === -1) {
+        listOfCategories.push("Destitution/NRPF");
+        }
+      }
+
+      if ( !event.target.checked && event.target.value === ('Destitution')){
+        if(listOfCategories.includes("Destitution/NRPF")) {        
+        index = listOfCategories.indexOf("Destitution/NRPF")
+        listOfCategories.splice(index, 1);
+        }
+      }
+
+      if ( !event.target.checked && event.target.value === ('Destitution')){
+        if(listOfCategories.includes('Destitution') || listOfCategories.includes('Destitution/NRPF')) {        
+          index = listOfCategories.indexOf('Destitution') || listOfCategories.indexOf('Destitution/NRPF');
+          listOfCategories.splice(index, 1);
+          } else {
+            //
+          }
+      } 
+
+      for(let i = 0; i < nonComposedCategories.length; i += 1) {
         // Case where category is checked and category's name is not composed
-        if(event.target.checked && !event.target.value.includes(composedCategories[i].split(' ').join(''))){
-
-          if(listOfCategories.indexOf(event.target.value) === -1) {
+        if(event.target.checked && event.target.value.includes(nonComposedCategories[i].split(' ').join(''))){
+          if(listOfCategories.indexOf(event.target.value) === -1 ) {
             listOfCategories.push(event.target.value);              
-            
           } 
-            else if(listOfCategories.indexOf(event.target.value) > -1) {
-              // Do nothing
-            }
       }
 
         // Case where category is not checked and category's name is not composed
-      if(!event.target.checked && !event.target.value.includes(composedCategories[i].split(' ').join(''))){
-
+      if(!event.target.checked && event.target.value.includes(nonComposedCategories[i].split(' ').join(''))){
            if(listOfCategories.indexOf(event.target.value) > -1) {
             index = listOfCategories.indexOf(event.target.value)
             listOfCategories.splice(index, 1)
           }
     }
-
+    
     } 
     this.setState({
       [event.target.name]: event.target.checked,
       Categories: [...new Set(listOfCategories)],
     });
-    
   };
 
-  handleDefaultCheckbox = event => {
-    
-    const listOfCategories = [...this.state.Categories];
-    let index;
-      for(let i = 0; i < composedCategories.length; i += 1) {
-        
-        // Case where category is checked and category's name is composed
-          if(event.target.checked && event.target.value.includes(composedCategories[i].split(' ').join(''))) {
-            if(listOfCategories.indexOf(composedCategories[i]) === -1) {
-              listOfCategories.push(composedCategories[i]);              
-            } else if (listOfCategories.indexOf(composedCategories[i]) > -1){
-              index = listOfCategories.indexOf(event.target.value)
-              listOfCategories.splice(index, 1) 
-            }
-        } 
-
-        // Case where category is not checked and category's name is composed
-        if(!event.target.checked && event.target.value.includes(composedCategories[i].split(' ').join(''))) { 
-          if(listOfCategories.indexOf(composedCategories[i]) > -1) {
-            index = listOfCategories.indexOf(composedCategories[i])
-            listOfCategories.splice(index, 1)             
-          }
-        } 
-
-        // Case where category is checked and category's name is not composed
-        if(event.target.checked && !event.target.value.includes(composedCategories[i].split(' ').join(''))){
-
-          if(listOfCategories.indexOf(event.target.value) === -1) {
-            listOfCategories.push(event.target.value);              
-            
-          } 
-            else if(listOfCategories.indexOf(event.target.value) > -1) {
-              // Do nothing
-            }
-      }
-
-        // Case where category is not checked and category's name is not composed
-      if(!event.target.checked && !event.target.value.includes(composedCategories[i].split(' ').join(''))){
-
-           if(listOfCategories.indexOf(event.target.value) > -1) {
-            index = listOfCategories.indexOf(event.target.value)
-            listOfCategories.splice(index, 1)
-          }
-    }
-    this.setState({
-      [event.target.name]: event.target.value,
-      Categories: [...new Set(listOfCategories)],
-      isChecked:!this.state.isChecked,
-    });
-  } 
-
-  };
-
+  
   handleMulitySelectChange = event => {
     this.setState({ Day: event.target.value });
   };
@@ -290,11 +330,7 @@ class EditOrganisation extends React.Component {
   };
 
   render() {
-    console.log('from render', this.state.Categories);
-    
     const checkedCategory = helpers.categoryNameMaker(this.props.location.pathname);
-    console.log(checkedCategory);
-    
     if (this.state.isLoading) {
       return <Spinner color='blue' bgColor='spinnerEdit' />;
     }
