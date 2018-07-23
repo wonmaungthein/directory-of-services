795a7713d64101ce62aa109d2ca1748786747b59
dwinatech <diwina1993@hotmail.com>
2018-07-23 12:57:55 +0100

    created initial test to UsersPag, AddUser, UsersTable, Notification, UserDropDown, UsersListTable, UsersMenu and UserTableHead components.

---
+++ frontend/src/_test_/_users_/Add_user.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import AddUser from '../../Components/Users/AddUser';
+
+configure({ adapter: new Adapter() });
+
+describe('AddUser Component', () => {
+    it('Should render without crashing', () => {
+        const wrapper = 
+        shallow(<AddUser />).length
+        expect(wrapper).toEqual(1)
+    })
+})
---
+++ frontend/src/_test_/_users_/Users_table_head.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import UsersTableHead from '../../Components/Users/UsersTableHead';
+
+configure({ adapter: new Adapter() });
+
+describe('UsersTableHead Component', () => {
+    it('Should render without crashing', () => {
+        const wrapper = 
+        shallow(<UsersTableHead />).length
+        expect(wrapper).toEqual(1)
+    })
+})
