aa476f7fe6f72bbd992d05b7dbbf7156c4e60336
dwinatech <diwina1993@hotmail.com>
2018-07-24 11:28:02 +0100

    Created initial test to SideBar, Spinner and TopNav components

---
+++ frontend/src/_test_/_top_nav_/Top_nav.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import TopNav from '../../Components/TopNav';
+
+configure({ adapter: new Adapter() });
+
+describe('TopNav Component', () => {
+    it('Should render without crashing', () => {
+        const wrapper = 
+        shallow(<TopNav />).length
+        expect(wrapper).toEqual(1)
+    })
+}) 
