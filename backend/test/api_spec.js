460431d6210cb302f7dc951f4876b13cdd6f9354
coderyourfuture <ibramohauk@gmail.com>
2018-08-18 12:41:31 +0100

    fix linting error and add validation for postcode API

--- backend/test/api_spec.js
+++ backend/test/api_spec.js
@@ -4,115 +4,73 @@ import { expect } from 'chai';
 import app from '../src/app';
 
 describe('API testing', async () => {
-  it('should return list of organisation in JSON format', done => {
-    request(app)
+  it('should return list of organisation in JSON format', async () => {
+    const result = await request(app)
       .get('/api/service/all')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then(response => {
-        expect(response.body).to.be.a('array');
-        done();
-      });
+    expect(result.body).to.be.a('array');
   });
 
-  it('should return list of categories in JSON format', done => {
-    request(app)
+  it('should return list of categories in JSON format', async () => {
+    const result = await request(app)
       .get('/api/service/categories')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then(response => {
-        expect(response.body).to.be.a('array');
-        done();
-      });
+    expect(result.body).to.be.a('array');
   });
 
-  it('should return list of boroughs in JSON format', done => {
-    request(app)
+  it('should return list of boroughs in JSON format', async () => {
+    const result = await request(app)
       .get('/api/service/boroughs')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then(response => {
-        expect(response.body).to.be.a('array');
-        done();
-      });
+    expect(result.body).to.be.a('array');
   });
 
-  it('should return list of areas in JSON format', done => {
-    request(app)
+  it('should return list of areas in JSON format', async () => {
+    const result = await request(app)
       .get('/api/service/areas')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then(response => {
-        expect(response.body).to.be.a('array');
-        done();
-      });
+    expect(result.body).to.be.a('array');
   });
 
-  it('should return list of boroughs filtered by category in JSON format', done => {
-    request(app)
+  it('should return list of boroughs filtered by category in JSON format', async () => {
+    const result = await request(app)
       .get('/api/service/category/?category=Education')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then(response => {
-        expect(response.body).to.be.a('array');
-        done();
-      });
+    expect(result.body).to.be.a('array');
   });
 
-  it('should return list of boroughs filtered by day in JSON format', done => {
-    request(app)
+  it('should return list of boroughs filtered by day in JSON format', async () => {
+    const result = await request(app)
       .get('/api/service/day/?day=Monday')
       .expect('Content-Type', /json/)
       .expect(200)
-      .then(response => {
-        expect(response.body).to.be.a('array');
-        done();
-      });
+    expect(result.body).to.be.a('array');
   });
 
-  it('should return list of boroughs filtered by borough in JSON format', done => {
-    request(app)
+  it('should return list of boroughs filtered by borough in JSON format', async () => {
+    const result = await request(app)
       .get('/api/service/borough/?borough=Bexley')
       .expect('Content-Type', /json/)
-      .expect(200)
-      .then(response => {
-        expect(response.body).to.be.a('array');
-        done();
-      });
+      .expect(200);
+    expect(result.body).to.be.a('array');
   });
 
-  it('should return list of boroughs filtered by borough in JSON format', done => {
-    request(app)
-      .get('/api/service/borough/?borough=Bexley')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then(response => {
-        expect(response.body).to.be.a('array');
-        done();
-      });
-  });
-
-  it('should return list of boroughs filtered by borough in JSON format', done => {
-    request(app)
-      .get('/api/service/borough/?borough=Bexley')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .then(response => {
-        expect(response.body).to.be.a('array');
-        done();
-      });
-  });
-
-  it.only('should not save unless correct data has been supplied', async () => {
-    const postcode = { postcod: 'W7 3DR' };
+  it('should not save unless correct data has been supplied', async () => {
+    const postcode = { lat: 'nw78', long: '657858' };
     const result = await request(app)
       .post('/api/service/postcode')
-      .send({ postcode });
+      .send(postcode);
     expect(result.statusCode).to.be.equal(200);
-    expect(result.body.success).to.be.true;
+    expect(result.body.success).to.be.equal(true)
     expect(result.body.message).to.be.equal('The Postcode has been saved successfully');
   });
-  it.only('should save with right data', async () => {
+
+  it('should save with right data', async () => {
     const anOrg = {
       organisation: 'testOrg',
       categories: 'Benefit'
@@ -121,7 +79,7 @@ describe('API testing', async () => {
       .post('/api/service/organisation/add')
       .send(anOrg);
     expect(result.statusCode).to.be.equal(200);
-    expect(result.body.success).to.be.true;
+    expect(result.body.success).to.be.equal(true);
     expect(result.body.message).to.be.equal('The organisation has been saved successfuly');
   });
 });
