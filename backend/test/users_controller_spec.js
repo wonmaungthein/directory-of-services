fd01e269a54a24ea62bec78448e33909000e606e
dwinatech <diwina1993@hotmail.com>
2018-08-20 12:35:44 +0100

    reordered  users controller test to make all test pass beside added one more test to make sure delete user test work correctly

--- backend/test/users_controller_spec.js
+++ backend/test/users_controller_spec.js
@@ -1,4 +1,6 @@
 import { expect } from 'chai'
+import request from 'supertest';
+import app from '../src/app'
 import {
   addUser,
   deleteUser,
@@ -9,6 +11,19 @@ import {
 } from '../src/controllers/users_controller'
 
 describe('Test users controller functions', () => {
+
+  it('Should add new user', async () => {
+    const userData = {
+      email: 'test@hotmail.com',
+      organisation: 'CYF',
+      fullname: 'test',
+      salt_password: '1234'
+    }
+    const result = await addUser(userData);
+    expect(result).to.be.a('object')
+    expect(result.email).to.equal(userData.email)
+  })
+
   it('Should get list of users and at least includes one user', async () => {
     const result = await getAllUsers();
     expect(result).to.be.a('array')
@@ -22,45 +37,38 @@ describe('Test users controller functions', () => {
     expect(result.id).to.equal(1)
   })
 
-  it('Should edit a user info', async () => {
-    const userData = {
-      email: 'mohammed@hotmail.com',
-      organisation: 'CYF',
-      fullname: 'Mohammed',
-      salt_password: '1234'
-    }
-    const userId = 4;
-    const result = await updateUser(userId, userData);
-    expect(result).to.be.a('number')
-    expect(result).to.equal(1)
-  })
-
-  it('Should delete a user info', async () => {
-    const userId = 3;
-    const result = await deleteUser(userId);
-    expect(result).to.be.a('number')
-    expect(result).to.equal(1)
-  })
-
   it('Should find a user info by using his/her email', async () => {
-    // you need to update the email as you expect this test only pass at my local code
-    // so you need to update this email you email expectation
-    const email = 'mohammed@hotmail.com';
+    const email = 'test@hotmail.com';
     const result = await getUserByEmail(email);
     expect(result).to.be.a('array')
     expect(result[0].email).to.equal(email)
   })
 
-  it('Should add new user', async () => {
+  it('Should edit a user info', async () => {
     const userData = {
       email: 'test@hotmail.com',
       organisation: 'CYF',
       fullname: 'test',
       salt_password: '1234'
     }
-    const result = await addUser(userData);
-    expect(result).to.be.a('object')
-    expect(result.email).to.equal(userData.email)
+    const userId = 1;
+    const result = await updateUser(userId, userData);
+    expect(result).to.be.a('number')
+    expect(result).to.equal(1)
+  })
+
+  it('Should delete a user info', async () => {
+    const userId = 1;
+    const result = await deleteUser(userId);
+    expect(result).to.be.a('number')
+    expect(result).to.equal(1)
+  })
+
+  it('Should return empty string for user his/her id 1', async () => {
+    const result = await request(app).get('/api/users/1')
+      .expect('Content-Type', /json/);
+    expect(result.body).to.be.a('string')
+    expect(result.body.length).to.equal(0)
   })
 
 })
