afde057139ef46641197a75ad373b9b8dc1a79bb
louiaR <louitiani@gmail.com>
2018-09-04 18:30:07 +0100

    Better result

--- frontend/src/Components/HomePage/homePageHelper.js
+++ frontend/src/Components/HomePage/homePageHelper.js
@@ -1,34 +1,37 @@
 import helpers from '../../helpers';
 
 const filterData = (orgs, search, postcodeValue) => {
-    if (search && postcodeValue) {
-      return orgs.filter(org => org.org_name.toLowerCase().includes(search.toLowerCase()) || org.borough.toLowerCase().includes(search.toLowerCase())
+    if (search  && postcodeValue ) {
+      return orgs.filter(org => org.org_name.toLowerCase().includes(search.trim().toLowerCase()) 
+        || org.borough.toLowerCase().includes(search.trim().toLowerCase())
         // return branches which service match or part of search
-        || search.toLowerCase().includes(org.service.toLowerCase())
+        || org.service.trim().toLowerCase().includes(search.toLowerCase())
         // return all branches which provide service on that day
-        || org.service_days.toLowerCase().includes(search.toLowerCase())
+        || org.service_days.toLowerCase().includes(search.trim().toLowerCase())
         // return all branches if search match cat_name
-        || org.cat_name.toLowerCase().includes(search.toLowerCase())
+        || org.cat_name.toLowerCase().includes(search.trim().toLowerCase())
         // return all branches if search tag match
-        || org.tag.toLowerCase().includes(search.toLowerCase())
+        || org.tag.toLowerCase().includes(search.trim().toLowerCase())
         // return all branches if search tag match
-        || org.area.toLowerCase().includes(search.toLowerCase())
+        || org.area.toLowerCase().includes(search.trim().toLowerCase())
       )
     } else if (search) {
-      return orgs.filter(org => org.org_name.toLowerCase().includes(search.toLowerCase()) || org.borough.toLowerCase().includes(search.toLowerCase())
+      return orgs.filter(org => org.org_name.toLowerCase().includes(search.trim().toLowerCase()) 
+        || org.borough.toLowerCase().includes(search.trim().toLowerCase())
         // return branches which service match or part of search
-        || search.toLowerCase().includes(org.service.toLowerCase())
+        || org.service.trim().toLowerCase().includes(search.toLowerCase())
         // return all branches which provide service on that day
-        || org.service_days.toLowerCase().includes(search.toLowerCase())
+        || org.service_days.toLowerCase().includes(search.trim().toLowerCase())
         // return all branches if search match cat_name
-        || org.cat_name.toLowerCase().includes(search.toLowerCase())
+        || org.cat_name.toLowerCase().includes(search.trim().toLowerCase())
         // return all branches if search tag match
-        || org.tag.toLowerCase().includes(search.toLowerCase())
+        || org.tag.toLowerCase().includes(search.trim().toLowerCase())
         // return all branches if search tag match
-        || org.area.toLowerCase().includes(search.toLowerCase())
+        || org.area.toLowerCase().includes(search.trim().toLowerCase())
       )
         .sort(helpers.sortArrObj)
-    } else if (postcodeValue) {
+    } 
+    else if (postcodeValue) {
       return orgs
     }
     return [];
